6a8f24f4aef7347da80423d5aac5220358c34622
apiwyayay <ravyardian13@gmail.com>
2024-01-24 23:50:25 +0700

    15

---
+++ 15-FindtheSmallestIntegerintheArray.py
@@ -0,0 +1,8 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/55a2d7ebe362935a210000b2/train/python
+
+def find_smallest_int(arr):
+    return min(arr)
+
+print(find_smallest_int([78, 56, 232, 12, 11, 43])) # 11
+print(find_smallest_int([78, 56, -2, 12, 8, -33])) # -33
