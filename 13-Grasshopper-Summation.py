ec62d9b6dc32b94608df7efa3defa35b945cf2a5
apiwyayay <ravyardian13@gmail.com>
2024-01-24 23:42:56 +0700

    13

---
+++ 13-Grasshopper-Summation.py
@@ -0,0 +1,8 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/python
+
+def summation(num):
+    return sum(i+1 for i in range(num))
+
+print(summation(1)) # 1
+print(summation(8)) #36
