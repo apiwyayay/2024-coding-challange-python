c12d85215c8f265ebdaf78f1dd867efff120e381
apiwyayay <ravyardian13@gmail.com>
2024-01-24 23:29:26 +0700

    9

---
+++ 9-ConvertNumbertoStrings.py
@@ -0,0 +1,9 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/5265326f5fda8eb1160004c8/train/python
+
+def number_to_string(num):
+    return str(num)
+
+print(number_to_string(67)) # '67'
+print(number_to_string(1+2)) # '3'
+print(number_to_string(1-2)) # '-1'
