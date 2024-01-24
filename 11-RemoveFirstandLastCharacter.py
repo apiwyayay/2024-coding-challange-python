f22b489b5e56c63586abdef93b2f801755252a29
apiwyayay <ravyardian13@gmail.com>
2024-01-24 23:35:30 +0700

    11

---
+++ 11-RemoveFirstandLastCharacter.py
@@ -0,0 +1,9 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/python
+
+def remove_char(s):
+    return s[1:-1]
+
+print(remove_char("eloquent")) # 'loquen'
+print(remove_char("country")) # 'ountr'
+print(remove_char("person")) # 'erso'
