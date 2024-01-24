b60cc6636fbe3ca06ab950b200e17b5b987e3b63
apiwyayay <ravyardian13@gmail.com>
2024-01-24 23:31:57 +0700

    10

---
+++ 10-OppositeNumber.py
@@ -0,0 +1,8 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/56dec885c54a926dcd001095/train/python
+
+def opposite(number):
+  return -number
+
+print(opposite(1)) # -1
+print(opposite(-2)) # 2
