74291821f839044585a450ad56929a05f314da9e
apiwyayay <ravyardian13@gmail.com>
2024-01-25 06:54:29 +0700

    16

---
+++ 16-BasicMathematicalOperations.py
@@ -0,0 +1,16 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/57356c55867b9b7a60000bd7/train/python
+
+def basic_op(operator, value1, value2):
+    if operator == '+':
+        return value1 + value2
+    elif operator == '-':
+        return value1 - value2
+    elif operator == '*':
+        return value1 * value2
+    elif operator == '/':
+        return value1 / value2
+
+print(basic_op('+', 4, 7)) # 11
+print(basic_op('-', 15, 18)) # -3
+print(basic_op('*', 5, 5)) # 25
