f17e1a0f9cba0b84c8d6a1f1a1fc987bfa3c28f2
apiwyayay <ravyardian13@gmail.com>
2024-01-25 06:59:08 +0700

    17

---
+++ 17-KeepHydrated.py
@@ -0,0 +1,8 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/582cb0224e56e068d800003c/train/python
+
+def litres(time):
+    return time // 2
+
+print(litres(3)) # 1
+print(litres(6.7)) # 3
