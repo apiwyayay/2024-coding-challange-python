cdaf89326ed79692c7cd397f29d9505f97e8f7fa
apiwyayay <ravyardian13@gmail.com>
2024-01-24 23:28:56 +0700

    5

---
+++ 5-ReturnNegative.py
@@ -0,0 +1,9 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/55685cd7ad70877c23000102/train/python
+
+def make_negative(number):
+    return -abs(number)
+
+print(make_negative(1)) # -1
+print(make_negative(-5)) # -5
+print(make_negative(0)) # 0
