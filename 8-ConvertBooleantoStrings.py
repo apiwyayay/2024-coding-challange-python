3eae839f658736f36feeb5503fe5ff38c11dc42f
apiwyayay <ravyardian13@gmail.com>
2024-01-24 23:29:19 +0700

    8

---
+++ 8-ConvertBooleantoStrings.py
@@ -0,0 +1,8 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python
+
+def bool_to_word(boolean):
+    return "Yes" if boolean else "No"
+
+print(bool_to_word(True)) # 'Yes'
+print(bool_to_word(False)) # 'No'
