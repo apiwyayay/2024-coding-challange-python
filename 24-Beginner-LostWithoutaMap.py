c701407ce65dd044780bf7ba5745cff2b762b1bb
apiwyayay <ravyardian13@gmail.com>
2024-01-25 14:09:28 +0700

    24

---
+++ 24-Beginner-LostWithoutaMap.py
@@ -0,0 +1,8 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/python
+
+def maps(a):
+    return [i*2 for i in a]
+
+print(maps([1, 2, 3])) # [2, 4, 6]
+print(maps([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])) # [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
