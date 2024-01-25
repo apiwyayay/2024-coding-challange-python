ff320ca13e8371dff13a9f94ea445255d58f6fae
apiwyayay <ravyardian13@gmail.com>
2024-01-25 07:21:59 +0700

    21

---
+++ 21-ReturningStrings.py
@@ -0,0 +1,8 @@
+# 2024 Coding Challenge
+# https://www.codewars.com/kata/55a70521798b14d4750000a4/train/python
+
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+
+print(greet("Ryan")) # "Hello, Ryan how are you doing today?"
+print(greet("Shingles")) # "Hello, Shingles how are you doing today?"
